71ffa5ed286ad3eaa1a913f925e8287d199b6552
Hijikata1869 <53096397+Hijikata1869@users.noreply.github.com>
2021-03-08 05:22:39 +0900

    分割代入

--- src/index.js
+++ src/index.js
@@ -14,13 +14,34 @@
 // console.log(val3);
 
 // // テンプレート文字列
-const name = "じゃけえ";
-const age = 28;
+// const name = "じゃけえ";
+// const age = 28;
 // 私の名前はじゃけぇです。年齢は２８歳です。
 // 従来の方法
-const message1 = "私の名前は" + name + "です。年齢は" + age + "です。";
-console.log(message1);
+// const message1 = "私の名前は" + name + "です。年齢は" + age + "です。";
+// console.log(message1);
 
-// テンプレート文字列を用いた方法
-const message2 = `私の名前は${name}です。年齢は${age}です。`;
-console.log(message2);
+// // テンプレート文字列を用いた方法
+// const message2 = `私の名前は${name}です。年齢は${age}です。`;
+// console.log(message2);
+
+// 分割代入
+// const myProfile = {
+//   name: "じゃけぇ",
+//   age: 28,
+// };
+
+// const message1 = `名前は${myProfile.name}です。年齢は${myProfile.age}歳です。`;
+// console.log(message1);
+
+// const { name, age } = myProfile;
+// const message2 = `名前は${name}です。年齢は${age}歳です。`;
+// console.log(message2);
+const myProfile = ["じゃけぇ", 28];
+
+const message3 = `名前は${myProfile[0]}です。年齢は${myProfile[1]}歳です。`;
+console.log(message3);
+
+const [name, age] = myProfile;
+const message4 = `名前は${name}です。年齢は${age}歳です。`;
+console.log(message4);
